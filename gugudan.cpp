c3f6227961599ab706cb85ed726fd69c253b49e7
SangWi Kang <ksw292002@khu.ac.kr>
2018-10-12 10:23:09 +0900

    commit for PR

--- gugudan.cpp
+++ gugudan.cpp
@@ -5,12 +5,14 @@ int main ()
 {
 	for (int i = 1; i < 9; i++)
 	{
-		cout << "=========" << i << "´Ü" << "=========" << endl;
+		cout << "=========" << i << "ï¿½ï¿½" << "=========" << endl;
 		for (int j = 1; j < 9; j++)
 		{
 			cout << i << " * " << j << " = " << i * j << endl;
 		}
 		cout << endl;
 	}
+
+	cout << "This is SANGWI/KANG" << endl;
 	return 0;
 }
